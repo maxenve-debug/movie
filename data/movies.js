f9803d68d9241dfc0761c15f1bfbb2132e4d23dd
maxenve-debug <beomgyuchoi1992@gmail.com>
2026-09-25 23:18:51 +0800

    Update movies.js

--- data/movies.js
+++ data/movies.js
@@ -45,7 +45,7 @@ const movies = [
     id: "barbie",
     title: "Barbie",
     year: 2023,
-    poster: "images/barbie2023/barbie2023.png",
+    poster: "images/posters/barbie2023.png",
     backdrop: "https://picsum.photos/seed/barbiebg/1280/720",
     synopsis: "Barbie and Ken are having the time of their lives in the colorful and seemingly perfect world of Barbie Land. When they get a chance to go to the real world, they soon discover the joys and perils of living among humans.",
     genres: ["Comedy", "Adventure", "Fantasy"],
